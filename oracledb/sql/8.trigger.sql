58e0d1fe88a59416def2ee07fab202fd49b566d9
Jmc <1934497212@qq.com>
2022-12-06 03:58:32 +0800

    Learn Oracle Trigger.

--- oracledb/sql/8.trigger.sql
+++ oracledb/sql/8.trigger.sql
@@ -1,15 +1,23 @@
 -- 触发器：一段被自动调用的代码，常用于更新后，插入前做一些操作
 
 -- 前置触发器
-create trigger before_insert before insert on t_user
+create or replace trigger before_insert before insert on t_user
+for each row
 begin
     dbms_output.put_line('t_user.before_insert trigger!');
+    -- 通过:new获取新插入的行
+    dbms_output.put_line('insert name -> ' || :new.name);
 end;
 
 -- 后置触发器
-create trigger after_update after update on t_user
+create or replace trigger after_update after update on t_user
+for each row
 begin
     dbms_output.put_line('t_user.after_update trigger!');
+    -- 通过:old获取旧行
+    dbms_output.put_line('old id -> ' || :old.id);
+    -- 通过:new获取新更新的行
+    dbms_output.put_line('new id -> ' || :new.id);
 end;
 
 -- 触发前置触发器
